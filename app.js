586784e820732d63b76ed8a76d12a7c5ce527e11
Hatsuya1993 <60688830+Hatsuya1993@users.noreply.github.com>
2020-07-23 08:08:01 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log("Listening to port " + port);
+});
---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//             $(window).scroll(() => {
+//                 let pos = $(this).scrollTop();
+//                 if (pos >= 483.20001220703125) {
+//                     $("").addClass("fixed-top");
+//                 } else {
+//                     $("").removeClass("fixed-top");
+//                 }
+//             });
