9fe4e970e9d4c8c0e4c5b48a1767e75df295aaec
Hatsuya1993 <60688830+Hatsuya1993@users.noreply.github.com>
2020-07-24 08:21:30 +0800

    carousel done

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,11 @@
 // $(document).ready(function () {
-//             $(window).scroll(() => {
-//                 let pos = $(this).scrollTop();
-//                 if (pos >= 483.20001220703125) {
-//                     $("").addClass("fixed-top");
-//                 } else {
-//                     $("").removeClass("fixed-top");
-//                 }
-//             });
+//     $(window).scroll(() => {
+//         let pos = $(this).scrollTop();
+//         console.log(pos)
+//         if (pos >= 483.20001220703125) {
+//             $("#nav2").addClass("fixed-top");
+//         } else {
+//             $("#nav2").removeClass("fixed-top");
+//         }
+//     });
+// })
