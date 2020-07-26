a25b4cb288cfdfbdfaef65f374e8cb62de6d6f71
Hatsuya1993 <60688830+Hatsuya1993@users.noreply.github.com>
2020-07-26 14:59:32 +0800

    projects done

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,25 @@
-// $(document).ready(function () {
-//     $(window).scroll(() => {
-//         let pos = $(this).scrollTop();
-//         console.log(pos)
-//         if (pos >= 483.20001220703125) {
-//             $("#nav2").addClass("fixed-top");
-//         } else {
-//             $("#nav2").removeClass("fixed-top");
-//         }
-//     });
-// })
+$(document).ready(function () {
+    $(window).scroll(() => {
+        let pos = $(this).scrollTop();
+        console.log(pos)
+        if (pos >= 121.5999984741211) {
+            $("#navbar").addClass("fixed-top");
+        } else {
+            $("#navbar").removeClass("fixed-top");
+        }
+    });
+
+
+    // init Isotope
+    let $grid = $('.grid').isotope({
+        // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+
+})
